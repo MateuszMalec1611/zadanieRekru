3159802f78382a0bf5feb6f6c54b9a6e515e5b2d
Mateusz Malec <m.mateusz2000@wp.pl>
2021-10-14 19:42:24 +0200

    feat: add icons for navlinks

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,20 +1,44 @@
-import { Nav } from 'react-bootstrap';
+import { Image, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import product from 'src/img/product.svg';
+import category from 'src/img/category.svg';
+import addProduct from 'src/img/addProduct.svg';
+import addCategory from 'src/img/addCategory.svg';
 
 const Sidebar = () => (
     <>
-        <Nav className="col-md-12 flex-column h-100 bg-dark" activeKey="/home">
-            <LinkContainer to="/" className="border-bottom">
-                <Nav.Link className="ps-2 text-light fs-5">Produkty</Nav.Link>
+        <Nav className="col-md-12 flex-column h-100 bg-dark fs-5" activeKey="/home">
+            <LinkContainer to="/" className="border-bottom ">
+                <Nav.Link className="text-light ">
+                    <div className="d-flex align-items-center">
+                        <Image className="me-2" src={product} />
+                        <p className="m-0 d-none d-md-block">Produkty</p>
+                    </div>
+                </Nav.Link>
             </LinkContainer>
             <LinkContainer to="/product-categories" className="border-bottom">
-                <Nav.Link className="ps-2 text-light fs-5">Kategorie</Nav.Link>
+                <Nav.Link className="text-light ">
+                    <div className="d-flex align-items-center">
+                        <Image className="me-2" src={category} />
+                        <p className="m-0 d-none d-md-block">Kategorie</p>
+                    </div>
+                </Nav.Link>
             </LinkContainer>
             <LinkContainer to="/add-product" className="border-bottom ">
-                <Nav.Link className="ps-2 text-light fs-5">Dodaj Produkt</Nav.Link>
+                <Nav.Link className="text-light ">
+                    <div className="d-flex align-items-center">
+                        <Image className="me-2" src={addProduct} />
+                        <p className="m-0 d-none d-none d-md-block">Dodaj Produkt</p>
+                    </div>
+                </Nav.Link>
             </LinkContainer>
             <LinkContainer to="/add-category" className="border-bottom ">
-                <Nav.Link className="ps-2 text-light fs-5">Dodaj Kategorię</Nav.Link>
+                <Nav.Link className="text-light ">
+                    <div className="d-flex align-items-center">
+                        <Image className="me-2" src={addCategory} />
+                        <p className="m-0 d-none d-md-block">Dodaj Kategorię</p>
+                    </div>
+                </Nav.Link>
             </LinkContainer>
         </Nav>
     </>
