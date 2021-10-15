1d2f04cfa514775a2da84ab89ec238d255982775
Mateusz Malec <m.mateusz2000@wp.pl>
2021-10-15 11:09:04 +0200

    fix: add category validation

--- src/pages/AddCategory/AddCategory.tsx
+++ src/pages/AddCategory/AddCategory.tsx
@@ -4,11 +4,15 @@ import PageTitle from 'src/components/PageTitle/PageTitle';
 import { useCategories } from 'src/hooks/useCategories';
 import { addCategory } from 'src/store/Categories/Categories.services';
 import { CategoriesActionType } from 'src/store/Categories/Categories.types';
+import { FormValidationError } from 'src/types/error.types';
+import { FormFieldNames } from 'src/types/form.types';
+import { translateErrorMessages } from 'src/utils/helpers';
 
 const AddCategory = () => {
     const [categoryName, setCategoryName] = useState('');
     const [onSuccess, setOnSuccess] = useState(false);
     const [error, setError] = useState({ isError: false, errorMessage: '' });
+    const [validationErrors, setValidationErrors] = useState<FormValidationError | undefined>();
     const {
         categoriesDispatch,
         categoriesState: { loading },
@@ -17,10 +21,6 @@ const AddCategory = () => {
     const handleAddCategory = async (event: React.FormEvent) => {
         event.preventDefault();
         setOnSuccess(false);
-        if (categoryName.trim() === '') {
-            setError({ isError: true, errorMessage: 'Nazwa musi być dłuższa niz jeden znak' });
-            return;
-        }
 
         try {
             categoriesDispatch({ type: CategoriesActionType.SET_LOADING });
@@ -31,16 +31,26 @@ const AddCategory = () => {
             categoriesDispatch({ type: CategoriesActionType.ADD_CATEGORY, payload: newCategory });
 
             setError({ isError: false, errorMessage: '' });
+            setValidationErrors(undefined);
             setOnSuccess(true);
             setCategoryName('');
         } catch (err: any) {
+            if (err.response?.status === 422) {
+                return setValidationErrors(err.response?.data);
+            }
             setError({ isError: true, errorMessage: err.message });
+        } finally {
             categoriesDispatch({ type: CategoriesActionType.SET_LOADING, payload: false });
         }
     };
 
     const handleCategoryName = ({ target }: React.ChangeEvent<HTMLInputElement>) =>
         setCategoryName(target.value);
+
+    const validateField = (name: string) =>
+        validationErrors?.errors.some((err: { field: string }) => err.field === name);
+    const getError = (name: string) =>
+        validationErrors?.errors.find((err: { field: string }) => err.field === name);
 
     return (
         <Container>
@@ -62,6 +72,13 @@ const AddCategory = () => {
                                     type="text"
                                     placeholder="Wpisz nazwę kategorii"
                                 />
+                                {validateField(FormFieldNames.NAME) && (
+                                    <p className="mb-0 mt-1 fs-6 text-danger">
+                                        {translateErrorMessages(
+                                            getError(FormFieldNames.NAME)?.code
+                                        )}
+                                    </p>
+                                )}
                             </Form.Group>
                             <Button className="w-max-content" variant="dark" type="submit">
                                 Zapisz
